cde1f782bab05d1a3a5214940679f4cf178795df
illusionyear <shuoli2010@foxmail.com>
2014-02-09 18:18:19 -0800

    A Error test.

--- CHelloWorld.c
+++ CHelloWorld.c
@@ -3,6 +3,10 @@
 int main(int argc, char *argv[]){
 	printf("Hello World, C!\n");
 	printf("Hello World, GitHub!\n");
+<<<<<<< .mine
+	printf("Hi.");
+=======
 	printf("Do a test!\n");
+>>>>>>> .r6
 	return 0;
 }
