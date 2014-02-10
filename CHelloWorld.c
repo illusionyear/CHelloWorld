6e77990ebd20788b726451035aae11944a2ad733
illusionyear <shuoli2010@foxmail.com>
2014-02-09 18:24:28 -0800

    Finally test.

--- CHelloWorld.c
+++ CHelloWorld.c
@@ -3,10 +3,5 @@
 int main(int argc, char *argv[]){
 	printf("Hello World, C!\n");
 	printf("Hello World, GitHub!\n");
-<<<<<<< .mine
-	printf("Hi.");
-=======
-	printf("Do a test!\n");
->>>>>>> .r6
 	return 0;
 }
